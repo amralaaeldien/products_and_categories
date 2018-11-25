f1ae43e2cfe7f473854e7c436e25c7eff2379834
amrshedou <amrshedou@gmail.com>
2018-11-26 01:10:21 +0200

    Update models.py

--- products_and_categories/models.py
+++ products_and_categories/models.py
@@ -3,7 +3,6 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
-	#id = models.PositiveIntegerField(primary_key=True)
 	name = models.CharField(max_length=255)
 	categoriesId = models.ForeignKey('self', related_name='categories',on_delete=models.CASCADE, blank=True, null=True)
 
@@ -11,7 +10,6 @@ class Category(models.Model):
 		return self.name
 
 class Product(models.Model):
-	#id = models.PositiveIntegerField(primary_key=True)
 	product_code = models.CharField(max_length=255)
 	name = models.CharField(max_length=255)
 	price = models.IntegerField()
