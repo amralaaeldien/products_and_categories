9d83d7e0ffa3f754c4c5cf1450362c8fd37b5ead
amrshedou <amrshedou@gmail.com>
2018-11-30 03:51:45 +0200

    fixed the many-to-many bug but got another bug which is the id one

--- products_and_categories/models.py
+++ products_and_categories/models.py
@@ -3,15 +3,15 @@ from django.db import models
 # Create your models here.
 
 class Category(models.Model):
-	#id = models.PositiveIntegerField(primary_key=True)
+	id = models.PositiveIntegerField(primary_key=True)
 	name = models.CharField(max_length=255)
-	categoriesId = models.ForeignKey('self', related_name='categories',on_delete=models.CASCADE, blank=True, null=True)
+	categories= models.ManyToManyField('self', related_name='categories', blank=True, null=True)
 
 	def __str__(self):
 		return self.name
 
 class Product(models.Model):
-	#id = models.PositiveIntegerField(primary_key=True)
+	id = models.PositiveIntegerField(primary_key=True)
 	product_code = models.CharField(max_length=255)
 	name = models.CharField(max_length=255)
 	price = models.IntegerField()
--- products_and_categories/serializers.py
+++ products_and_categories/serializers.py
@@ -2,25 +2,6 @@ from rest_framework import serializers
 from products_and_categories.models import Product, Category
 from django.db import models
 
-'''class CreatableSlugRelatedField(serializers.SlugRelatedField):
-	"""
-	Django rest frameork doesn't have a creatble slug field
-	so i built one that extends the slug field. it uses djnago
-	get_or_create function
-	"""
-	class Meta:
-		model = Category
-		fields = ( "name", 'products', 'categories')
-
-	def to_internal_value(self, data):
-		try:
-			return self.get_queryset().get_or_create(**{self.slug_field: data})[0]
-		except models.ObjectDoesNotExist:
-			self.fail('does_not_exist', slug_name=self.slug_field,
-					value=smart_text(data))
-		except (TypeError, ValueError):
-			self.fail('invalid')
-'''
 class CategorySerializer(serializers.ModelSerializer):
 	def to_representation(self, obj):
 		if 'categories' not in self.fields:
@@ -29,18 +10,14 @@ class CategorySerializer(serializers.ModelSerializer):
 
 	class Meta:
 		model = Category
-		fields = ("name", 'products', 'categories')
+		fields = ('id', "name", 'products', 'categories')
 	
 class ProductSerializer(serializers.ModelSerializer):
 	categories = CategorySerializer(many=True, required=False)
-	'''	categories = CreatableSlugRelatedField(many=False,
-											queryset=Category.objects.all(),
-											slug_field='categories',
-											required=False)
-	'''
+
 	class Meta:
 		model = Product
-		fields = ( "product_code", "name", "quantity", "price", 'categories')
+		fields = ('id', "product_code", "name", "quantity", "price", 'categories')
 
 	def update(self, instance, validated_data):
 		if 'categories' in validated_data:
@@ -56,11 +33,15 @@ class ProductSerializer(serializers.ModelSerializer):
 		return instance
 
 	def create(self, validated_data):
-		category_data = validated_data.get('categories')
+		category_data = validated_data.pop('categories')
 		product = Product.objects.create(**validated_data)
-		if category_data:
-			for category in category_data:
-				product.categories.create(**category)
+		dicti={}
+		for items in category_data:
+			for key, value in items.items() :
+				dicti[key] = value
+		if 'id' in dicti:
+			x= product.categories.get_or_create(id = dicti['id'])
+			product.categories.set(x)
 		return product
 
 
